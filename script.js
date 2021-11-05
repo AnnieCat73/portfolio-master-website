1d57d275f95fcd7019ad9e8a32180753c17d329c
annie_i <anniecats673@gmail.com>
2021-11-05 18:42:45 +0000

    Most links added

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-//Menu on Mobile up to min width: 35em
+//Menu on Mobile up to min width: 45em
 
 //Get Elements
 
 const openBtn = document.querySelector(".open-nav");
 const navMenu = document.querySelector(".menu-nav-mobile");
 const closeBtn = document.querySelector(".close-nav");
+
+const aboutLink = document.querySelector("#js-about");
+const skillsLink = document.querySelector("#js-skills");
+const projectsLink = document.querySelector("#js-projects");
+const contactLink = document.querySelector("#js-contact");
 
 //Open the menu
 
@@ -14,6 +19,27 @@ openBtn.addEventListener("click", () => {
 
 //Close the menu
 closeBtn.addEventListener("click", () => {
+  navMenu.classList.remove("navigation-open");
+});
+
+
+//Get to about section
+aboutLink.addEventListener("click", () => {
+  navMenu.classList.remove("navigation-open");
+});
+
+//Get to skills section
+skillsLink.addEventListener("click", () => {
+  navMenu.classList.remove("navigation-open");
+});
+
+//Get to projects section
+projectsLink.addEventListener("click", () => {
+  navMenu.classList.remove("navigation-open");
+});
+
+//Get to contact section
+contactLink.addEventListener("click", () => {
   navMenu.classList.remove("navigation-open");
 });
 
